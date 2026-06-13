df857bc37323146363578a138997bc5dd2f28ba3
Ann Molochko <molochkoannav@gmail.com>
2026-06-13 10:07:35 +0300

    reports module done

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import pandas as pd
+from src.reports import spending_by_category
 from src.services import search_by_phrases, search_by_phone_number, filters_transactions_by_persons
 from src.views import main_info
+from config import EXCEL_FILE_PATH
 
 if __name__ == "__main__":
     # print(main_info("2019-01-21 00:03:00"))
@@ -7,4 +10,6 @@ if __name__ == "__main__":
     # print(search_by_phone_number("921112233"))
     # print(search_by_phone_number("79955555555"))
     # print(search_by_phone_number("89813334455"))
-    print(filters_transactions_by_persons("Валерий А."))
+    # print(filters_transactions_by_persons("Валерий А."))
+    df = pd.read_excel(EXCEL_FILE_PATH)
+    print(spending_by_category(df, "Супермаркеты", "31.12.2021"))
--- src/views.py
+++ src/views.py
@@ -5,11 +5,37 @@ from src.utils import get_date_period, get_filtred_info, get_top_five, get_read_
 from src.utils import get_read_excel_file
 from src.utils import get_user_time
 from config import EXCEL_FILE_PATH,JSON_SETTINGS_PATH
+import logging
+from pathlib import Path
+
+current_file = Path(__file__)
+project_root = current_file.parent.parent
+log_dir = project_root / "logs"
+log_dir.mkdir(exist_ok=True)
+
+log_file_vs = log_dir / "views.log"
+
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logger_vs = logging.getLogger("views")
+logger_vs.setLevel(logging.DEBUG)
+
+
+file_handler_vs = logging.FileHandler(log_file_vs, mode="w", encoding="utf-8")
+file_handler_vs.setLevel(logging.DEBUG)
+
+
+formatter = logging.Formatter("%(asctime)s - %(funcName)s - %(levelname)s: %(message)s")
+file_handler_vs.setFormatter(formatter)
+
+
+logger_vs.addHandler(file_handler_vs)
+logger_vs.propagate = False
 
 
 def main_info(date_time: str)-> Dict[str, Any]:
     """Главная функция, принимающая на вход строку с датой и временем в формате
     YYYY-MM-DD HH:MM:SS и возвращающая JSON-ответ"""
+    logger_vs.info("Запуск функции main_info")
     greetings = get_user_time()
     date_period = get_date_period(date_time)
     sort_by_date_transactions = get_read_excel_file(EXCEL_FILE_PATH,date_period)
@@ -19,6 +45,7 @@ def main_info(date_time: str)-> Dict[str, Any]:
     currency_rates = get_currency_rates(currency_read)
     stock_data = get_stocks(currency_read)
     stock_prices = get_stocks_info(stock_data)
+    logger_vs.info("Функция отработала")
     data_info = {"greetings": greetings,
                  "cards": filter_cards_info,
                  "top_transactions": filter_top_transactions,
